cd804c9451fd9cf83207d5bbe1812e5efbc57bec
Md Saddam Hossain <eyarinsultana@gmail.com>
2026-04-30 16:54:20 +0600

    banner section added

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import Image from "next/image";
+import React from "react";
+const im = "https://www.kajariaceramics.com/storage/banner/desktop-banner.webp";
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="relative">
+      <Image
+        src={im}
+        alt="image"
+        width={1000}
+        height={800}
+        className="w-full"
+      />
+      <button className="btn absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">Browse Now</button>
+    </div>
+  );
 };
 
 export default Banner;
