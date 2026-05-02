db774924e25c10c74585ea315be78ab2753cadba
Saddam <eyarinsultana@gmail.com>
2026-05-02 15:46:34 +0600

    fetchData function updated

--- src/helper/fetchData.tsx
+++ src/helper/fetchData.tsx
@@ -1,8 +1,14 @@
 import { Tile } from "@/types/tile";
 
 export const fetchTilesData = async () => {
-  const data = await fetch(`${process.env.BETTER_AUTH_URL}/tiles_data.json`);
-  if (!data) return null;
-  const tiles: Tile[] = await data.json();
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+  if (!baseUrl) throw new Error("Missing APP_URL");
+
+  const res = await fetch(`${baseUrl}/tiles_data.json`);
+
+  if (!res.ok) return null;
+
+  const tiles: Tile[] = await res.json();
   return tiles;
 };
