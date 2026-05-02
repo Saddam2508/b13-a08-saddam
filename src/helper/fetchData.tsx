f9e50152954ce653526127fb3f7231097e9f51f1
Saddam <eyarinsultana@gmail.com>
2026-05-02 15:35:31 +0600

    local host url setup

--- src/helper/fetchData.tsx
+++ src/helper/fetchData.tsx
@@ -1,8 +1,8 @@
 import { Tile } from "@/types/tile";
 
 export const fetchTilesData = async () => {
-  const data = await fetch("http://localhost:3000/tiles_data.json");
-  if(!data) return null
+  const data = await fetch(`${process.env.BETTER_AUTH_URL}/tiles_data.json`);
+  if (!data) return null;
   const tiles: Tile[] = await data.json();
   return tiles;
 };
