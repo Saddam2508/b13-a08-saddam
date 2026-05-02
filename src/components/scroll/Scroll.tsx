c5950305724c52db45f8bbbd9d0a8c38da099f31
Saddam <eyarinsultana@gmail.com>
2026-05-02 14:54:20 +0600

    scroll page  updated

--- src/components/scroll/Scroll.tsx
+++ src/components/scroll/Scroll.tsx
@@ -1,15 +1,15 @@
-
-import Marquee from 'react-fast-marquee';
+import Marquee from "react-fast-marquee";
 
 const Scroll = () => {
-    return (
-        <div className='py-3 flex gap-1'>
-            <span className='btn text-sm sm:text-xl bg-amber-50'>New Arrivals:</span>
-            <Marquee className=' text-sm sm:text-xl' speed={90}>
-                 [Tile Name] | Weekly Feature: Modern Geometric Patterns | Join the Community...
-            </Marquee>
-        </div>
-    );
+  return (
+    <div className="py-3 flex gap-1">
+      <span className="btn text-xs sm:text-lg bg-amber-50">New Arrivals:</span>
+      <Marquee className=" text-xs sm:text-lg" speed={90}>
+        [Tile Name] | Weekly Feature: Modern Geometric Patterns | Join the
+        Community...
+      </Marquee>
+    </div>
+  );
 };
 
 export default Scroll;
